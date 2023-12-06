8308f01ca055474d87feff2fb47b37b85f1590cf
Adam Zhang <898544@lcps.org>
2023-12-06 18:39:50 -0500

    feat: use mold linker

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@
 with pkgs;
   mkShell {
     nativeBuildInputs = [rustc cargo cmake];
-    buildInputs = [rust-analyzer rustfmt pkg-config fontconfig];
+    buildInputs = [rust-analyzer rustfmt pkg-config fontconfig mold];
     LD_LIBRARY_PATH = lib.makeLibraryPath [wayland libxkbcommon];
   }
